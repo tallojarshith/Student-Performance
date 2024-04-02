f1fdbbe460325547109855c07d44964bead4ab70
Harshith <tallojarshith123@gmail.com>
2024-04-02 12:25:09 +0530

    model is done

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 import dill 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path,obj):
     try:
@@ -13,7 +14,7 @@ def save_object(file_path,obj):
         os.makedirs(dir_path,exist_ok=True)
 
         with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj,file_obj)
 
     except Exception as e:
         raise CustomException(e,sys)
@@ -37,5 +38,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             test_model_score=r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]]=test_model_score
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
